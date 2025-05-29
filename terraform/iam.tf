6a5239fd01bb225ae62771ba181118aa483f133b
NAVEEN <132891742+naveenj18@users.noreply.github.com>
2025-05-29 09:48:38 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -17,4 +17,4 @@ resource "aws_iam_role_policy_attachment" "ecs_execution_attach" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
-#
+
